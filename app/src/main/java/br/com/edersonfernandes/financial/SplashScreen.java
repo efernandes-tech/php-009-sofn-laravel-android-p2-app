9f068457dcd8ae928feba3b8131dfe01b6fbca26
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-20 17:20:24 -0300

    criando splash screen

---
+++ app/src/main/java/br/com/edersonfernandes/financial/SplashScreen.java
@@ -0,0 +1,24 @@
+package br.com.edersonfernandes.financial;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity implements Runnable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+
+    public void run() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+}
